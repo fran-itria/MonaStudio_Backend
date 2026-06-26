3709fdbc730edc977b8be849459ce83d578f3189
fran-itria <francoitria01@gmail.com>
2026-06-26 05:21:51 -0300

    feat(payments): remove Mercado Pago configuration file

--- src/mercadopago/mercadopago.config.ts
+++
@@ -1,5 +0,0 @@
-import { MercadoPagoConfig, Preference } from "mercadopago";
-
-console.log('Access token: ', process.env.ACCESS_TOKEN_SANDBOX);
-const client = new MercadoPagoConfig({ accessToken: process.env.ACCESS_TOKEN_SANDBOX! });
-export const preference = new Preference(client)

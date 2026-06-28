8cc35d05f73272a5c0858bc138fcbb1ac6fcbd26
fran-itria <francoitria01@gmail.com>
2026-06-28 04:46:14 -0300

    feat: implement findAllLessons and findOne methods in LessonsController and LessonsService

--- src/modules/lesson/lessons.controller.ts
+++ src/modules/lesson/lessons.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { LessonsService } from './lessons.service';
+import type { Response } from 'express';
+import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 
 @Controller('lessons')
 export class LessonsController {
-  constructor(private readonly lessonsService: LessonsService) {}
+  constructor(private readonly lessonsService: LessonsService) { }
+
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: "Se obtiene lista de modulos",
+    example: [
+      {
+        id: "uuid-módulo-1",
+        title: "Módulo 1",
+        content: {
+          title: "Introducción al desarrollo web",
+          description: "Javascript, HTML y Css"
+        }
+      }
+    ]
+  })
+  @Get()
+  async findAllLessons(@Res() res: Response) {
+    const lessons = await this.lessonsService.findAll()
+    return res.status(200).json(lessons)
+  }
+
+  @Get()
+  async findOne(@Res() res: Response) {
+    const lessons = await this.lessonsService.findAll()
+    return res.status(200).json(lessons)
+  }
 }
--- src/modules/lesson/lessons.service.ts
+++ src/modules/lesson/lessons.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lesson } from './entities/lesson.entity';
+import { Repository } from 'typeorm';
+import { ErrorsExceptions } from '../../Errors/custom-errors-exceptions';
+import { LessonsErrors } from '../../Errors/lessons.errors';
 
 @Injectable()
-export class LessonsService {}
+export class LessonsService {
+    constructor(
+        @InjectRepository(Lesson)
+        private readonly lessonsRepository: Repository<Lesson>
+    ) { }
+
+    async findAll() {
+        const lessons = await this.lessonsRepository.find()
+        if (lessons.length == 0) throw ErrorsExceptions.notFound(LessonsErrors.LESSONS_NOT_FOUND.errorCode, LessonsErrors.LESSONS_NOT_FOUND.message)
+        return lessons
+    }
+}

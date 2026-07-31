b932d962191098f047ddc056d7ffb0da27d10b39
fran-itria <francoitria01@gmail.com>
2026-07-31 03:06:33 -0300

    feat: add types for stock reduction and product selections to enhance order management

---
+++ src/modules/orders/types.ts
@@ -0,0 +1,29 @@
+import { EntityManager } from "typeorm";
+import { Product } from "../products/entities/product.entity";
+
+export interface ReduceStockProps {
+    products: {
+        id: string;
+        quantity?: number | undefined;
+        varityId?: {
+            id: "string";
+            quantity: number;
+        }[] | undefined;
+    }[],
+    manager: EntityManager
+}
+
+export interface SelectionsProps {
+    varityId: {
+        id: "string";
+        quantity: number;
+    }[] | undefined;
+    components: Product["components"];
+    productVarities: Product["productVarities"];
+    quantity: number
+}
+
+export interface ProductsPrices {
+    price: number,
+    discountedPrice: number | null
+}

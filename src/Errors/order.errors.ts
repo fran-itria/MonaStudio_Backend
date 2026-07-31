9e030b813a903b4839e56cd9137cf611e3f64da0
fran-itria <francoitria01@gmail.com>
2026-07-30 23:16:27 -0300

    feat: add error handling for order selections and product not found scenarios

--- src/Errors/order.errors.ts
+++ src/Errors/order.errors.ts
@@ -5,4 +5,5 @@ export class OrderErrors {
     static readonly BAD_REQUEST_PHONE = { message: "Se requiere un teléfono de contacto", errorCode: "BAD_REQUEST" }
     static readonly BAD_REQUEST_PRODUCTS = { message: "Se requiere mínimo un producto para el pedido", errorCode: "BAD_REQUEST" }
     static readonly BAD_REQUEST_SHIPPING = { message: "Los datos de envío son obligatorios", errorCode: "BAD_REQUEST" }
+    static readonly BAD_REQUEST_SELECTIONS = (quantity: number) => ({ message: `Debes elegir ${quantity} variedades`, erroCode: "BAD_REQUEST" })
 }
--- src/Errors/product.errors.ts
+++ src/Errors/product.errors.ts
@@ -3,5 +3,5 @@
 
 export class PorductErrors {
     static readonly NOT_FOUND_PRODUCT = { message: "No se encontró el producto", errorCode: "NOT_FOUND_PRODUCT" }
-
+    static readonly NOT_FOUND_ANY_PRODUCT = { message: "No se encontró alguno de los producto", errorCode: "NOT_FOUND_ANY_PRODUCT" }
 }
